6e1228989f43451d894b4dd8a22a6ed0392b0bd0
pamims <105949875+pamims@users.noreply.github.com>
2022-05-21 16:40:55 -0400

    "Create Band Name Generator main.py"
    Day 1 Project: Band Name Generator
    This program asks for input from the user to generate a band name.

---
+++ Day001 - Band Name Generator/main.py
@@ -0,0 +1,8 @@
+# Band Name Generator
+# Author: Powell A. Mims
+# This program asks for input from the user to generate a band name
+
+print("Welcome to the Band Name Generator!\n");
+city_name = input("What is the name of the city you grew up in?\n");
+pet_name = input("What is the name of your pet?\n");
+print("\nYour band name is: " + city_name + " " + pet_name);
