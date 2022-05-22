122c2ef28a956cade4cbe770615f80183e24c32d
Powell Mims <powellamims@gmail.com>
2022-05-22 17:10:10 -0400

    Create BMI Calculator; Change folder organization

---
+++ Day002/BMI_Calculator/bmi.py
@@ -0,0 +1,12 @@
+METERS_PER_FOOT = 0.3048;
+KG_PER_POUND = 0.453952;
+
+height = int(input("What is your height in feet?   "));
+weight = int(input("What is your weight in pounds? "));
+
+height *= METERS_PER_FOOT;
+weight *= KG_PER_POUND;
+
+bmi = weight / height ** 2;
+
+print(f"Your BMI is {bmi}");
