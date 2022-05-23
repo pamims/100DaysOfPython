dd40a611727ba042454b3a1a1bb8549eb4e2c555
Powell Mims <powellamims@gmail.com>
2022-05-23 18:28:50 -0400

    Improve tip_calc.py output

--- Day002/Tip_Calculator/tip_calc.py
+++ Day002/Tip_Calculator/tip_calc.py
@@ -2,13 +2,15 @@
 # Author: Powell A. Mims
 # This program calculates how much each person should pay for a shared order
 
-num_people = input("How many people are splitting the bill?\n    ");
-cost = input("What is the order cost?\n    ");
-tip_percent = input("What percent tip should be added?\n    ");
+print("Welcome to the Tip Calculator!\n");
+
+num_people = input("How many people are splitting the bill? ");
+cost = input("What is the order cost? $");
+tip_percent = input("What percent tip should be added? (10, 12, 15?) ");
 
 num_people = int(num_people);
 tip_percent = float(tip_percent) / 100;
 cost = float(cost)
-cost_per_person = cost * (1 + tip_percent) / num_people;
+cost_per_person = round(cost * (1 + tip_percent) / num_people, 2);
 
 print(f"Each person should pay ${cost_per_person}.");
