8ae7fb50028efc32cffe8e6f7190ffc914a6b4a5
Powell Mims <powellamims@gmail.com>
2022-05-23 22:41:55 -0400

    Create Coin Toss program

---
+++ Day004/Coin_Toss/coin_toss.py
@@ -0,0 +1,12 @@
+# Coin Toss
+# Author: Powell A. Mims
+# This program randomly chooses heads or tails
+
+import random;
+
+side = random.randint(0, 1);
+
+if side == 1:
+    print("Heads");
+else:
+    print("Tails");
