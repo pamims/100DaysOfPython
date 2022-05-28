c936882a8b26fe37d12448752c8022130807ba06
Powell Mims <powellamims@gmail.com>
2022-05-28 14:03:10 -0400

    Refactor code for readability, add comments

--- Day007/Hangman/hangman.py
+++ Day007/Hangman/hangman.py
@@ -1,4 +1,11 @@
+# Hangman
+# Author: Powell A. Mims
+# Play a game of hangman
+
+
 import random;
+
+# 'Constant' values
 WORDS = ["buffalo", "parrot", "squirrel", "hedgehog"];
 STAGES = ['''
   +---+
@@ -58,6 +65,7 @@ STAGES = ['''
 ''']
 ALLOWED_FAILURES = len(STAGES) - 1;
 
+# Helper functions
 def draw_output(correct_letters, incorrect_letters):
     stage = ALLOWED_FAILURES - len(incorrect_letters);
     print(f"The word so far: {correct_letters}");
@@ -65,15 +73,20 @@ def draw_output(correct_letters, incorrect_letters):
     print(f"Incorrect guesses: {incorrect_letters}");
     return;
 
-
+# Program start
 word = random.choice(WORDS);
-
 correct_letters = ['_'] * len(word);
 incorrect_letters = [];
-while '_' in correct_letters and len(incorrect_letters) < ALLOWED_FAILURES:
+game_is_over = False;
+
+# Game loop
+while not game_is_over:
+    # Show game status and collect input
     draw_output(correct_letters, incorrect_letters);
     guess = input("Guess a letter: ").lower();
     print("\n\n");
+
+    # Filter for bad input
     if guess in correct_letters or guess in incorrect_letters:
         print("That letter has already been used.");
         continue;
@@ -81,6 +94,7 @@ while '_' in correct_letters and len(incorrect_letters) < ALLOWED_FAILURES:
         print("That is not a letter.");
         continue;
 
+    # Update game state
     i = 0;
     if guess in word:
         for letter in word:
@@ -89,7 +103,12 @@ while '_' in correct_letters and len(incorrect_letters) < ALLOWED_FAILURES:
             i += 1;
     else:
         incorrect_letters.append(guess);
+    
+    # Check for game over
+    if '_' not in correct_letters or len(incorrect_letters) >= ALLOWED_FAILURES:
+        game_is_over = True;
 
+# Results
 draw_output(correct_letters, incorrect_letters);
 print("Game Over");
 if len(incorrect_letters) < ALLOWED_FAILURES:
