12cadb883fc24bf63a298ab7af2647faf3eccd21
Powell Mims <powellamims@gmail.com>
2022-05-28 13:52:14 -0400

    Add hangman stages ascii art

--- Day007/Hangman/hangman.py
+++ Day007/Hangman/hangman.py
@@ -1,21 +1,79 @@
 import random;
+WORDS = ["buffalo", "parrot", "squirrel", "hedgehog"];
+STAGES = ['''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ / \  |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ /    |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|   |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+  |   |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+      |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+      |
+      |
+      |
+      |
+=========
+''']
+ALLOWED_FAILURES = len(STAGES) - 1;
 
 def draw_output(correct_letters, incorrect_letters):
+    stage = ALLOWED_FAILURES - len(incorrect_letters);
     print(f"The word so far: {correct_letters}");
+    print(STAGES[stage]);
     print(f"Incorrect guesses: {incorrect_letters}");
     return;
 
-ALLOWED_FAILURES = 6;
-words = ["buffalo", "parrot", "squirrel", "hedgehog"];
-word = random.choice(words);
+
+word = random.choice(WORDS);
 
 correct_letters = ['_'] * len(word);
 incorrect_letters = [];
-
-letters_remaining = len(word);
-while letters_remaining > 0 and len(incorrect_letters) < ALLOWED_FAILURES:
+while '_' in correct_letters and len(incorrect_letters) < ALLOWED_FAILURES:
     draw_output(correct_letters, incorrect_letters);
     guess = input("Guess a letter: ").lower();
+    print("\n\n");
     if guess in correct_letters or guess in incorrect_letters:
         print("That letter has already been used.");
         continue;
@@ -24,17 +82,15 @@ while letters_remaining > 0 and len(incorrect_letters) < ALLOWED_FAILURES:
         continue;
 
     i = 0;
-    is_in_word = False;
-    for letter in word:
-        if guess == letter:
-            is_in_word = True;
-            correct_letters[i] = letter;
-            letters_remaining -= 1;
-        i += 1;
-    
-    if not is_in_word:
+    if guess in word:
+        for letter in word:
+            if guess == letter:
+                correct_letters[i] = letter;
+            i += 1;
+    else:
         incorrect_letters.append(guess);
 
+draw_output(correct_letters, incorrect_letters);
 print("Game Over");
 if len(incorrect_letters) < ALLOWED_FAILURES:
     print("Player wins!");
